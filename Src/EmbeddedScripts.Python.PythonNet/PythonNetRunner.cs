9c60401313e4d5b0cab46f667432429d1f1da7dc
Egor Medvedev <Egor.Medvedev@confirmit.com>
2021-08-25 11:15:49 +0300

    Add REPL in pythonnet runner

--- Src/EmbeddedScripts.Python.PythonNet/PythonNetRunner.cs
+++ Src/EmbeddedScripts.Python.PythonNet/PythonNetRunner.cs
@@ -1,36 +1,70 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using EmbeddedScripts.Shared;
+using EmbeddedScripts.Shared.Exceptions;
 using Python.Runtime;
 
 namespace EmbeddedScripts.Python.PythonNet
 {
-    public class PythonNetRunner : ICodeRunner
+    public class PythonNetRunner : ICodeRunner, IEvaluator, IDisposable
     {
-        private readonly Container _container = new();
-
+        private readonly PyScope _scope;
+        
         public static string PythonDll 
         {
             get => Runtime.PythonDLL;
             set => Runtime.PythonDLL = value;
         }
+
+        public PythonNetRunner()
+        {
+            using (Py.GIL())
+                _scope = Py.CreateScope();
+        }
         
         public Task RunAsync(string code)
         {
-            using (new PythonMultithreadingScope())
             using (Py.GIL())
-            using (var scope = Py.CreateScope())
-                scope
-                    .AddHostObjectsFromContainer(_container)
-                    .Exec(code);
+            {
+                try
+                {
+                    _scope.Exec(code);
+                }
+                catch (PythonException e)
+                {
+                    throw e.Type.Name switch
+                    {
+                        ErrorCodes.SyntaxError or ErrorCodes.IndentationError or ErrorCodes.TabError =>
+                            new ScriptSyntaxErrorException(e.Message, e),
+                        ErrorCodes.SystemError or ErrorCodes.OsError or ErrorCodes.SystemExit =>
+                            new ScriptEngineErrorException(e.Message, e),
+                        _ => new ScriptRuntimeErrorException(e.Message, e)
+                    };
+                }
+            }
 
             return Task.CompletedTask;
         }
 
         public ICodeRunner Register<T>(T obj, string alias)
         {
-            _container.Register(obj, alias);
-
+            using (Py.GIL())
+                _scope.Set(alias, obj);
+            
             return this;
+        }
+
+        public Task<T> EvaluateAsync<T>(string expression)
+        {
+            using (Py.GIL())
+                return Task.FromResult(_scope.Eval<T>(expression));
+        }
+        
+        public void Dispose()
+        {
+            using (new PythonMultithreadingScope()) // TODO Workaround for System.InvalidOperationException: This property must be set before runtime is initialized at Python.Runtime.Runtime.set_PythonDLL(String value)
+            using (Py.GIL())
+                _scope?.Dispose();
         }
     }
 }
--- Tests/EmbeddedScripts.Python.PythonNet.Tests/PythonNetRunnerTests.cs
+++ Tests/EmbeddedScripts.Python.PythonNet.Tests/PythonNetRunnerTests.cs
@@ -1,6 +1,9 @@
  using System;
+ using System.Net.Sockets;
  using System.Runtime.InteropServices;
+ using System.Text;
  using System.Threading.Tasks;
+ using EmbeddedScripts.Shared.Exceptions;
  using HelperObjects;
  using Xunit;
  using Python.Runtime;
@@ -14,59 +17,59 @@ namespace EmbeddedScripts.Python.PythonNet.Tests
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
                 PythonNetRunner.PythonDll = "/usr/lib/python3.8/config-3.8-x86_64-linux-gnu/libpython3.8.so";
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                PythonNetRunner.PythonDll = "python38.dll";
+                PythonNetRunner.PythonDll = "C:/Python38/python38.dll";
         }
 
         [Fact]
-        public async void RunValidCode_Succeed()
+        public async void RunAsync_RunValidCode_Succeed()
         {
             var code = @"
 a = 1 
 b = 2
 c = a + b";
 
-            var runner = new PythonNetRunner();
+            using var runner = new PythonNetRunner();
 
             await runner.RunAsync(code);
         }
 
         [Fact]
-        public async void RunWithGlobalVariables_Succeed()
+        public async void RunAsync_RunWithGlobalVariables_Succeed()
         {
             var t = new HelperObject();
             var code = "t.x += 1;";
 
-            var runner = new PythonNetRunner();
+            using var runner = new PythonNetRunner();
             runner.Register(t, "t");
 
             await runner.RunAsync(code);
 
             Assert.Equal(1, t.x);
         }
-
+        
         [Fact]
-        public async Task AddConfigOnce_SetsConfig_Succeed()
+        public async Task Register_TryToAccessUnregisteredVar_ThrowsException()
         {
-            var t = new HelperObject();
-            var code = "t.x += 1;";
+            var s = "abc";
+            var code = "t.x += len(s)";
 
-            var runner = new PythonNetRunner();
-            runner.Register(t, "t");
+            using var runner = new PythonNetRunner();
+            runner.Register(s, "s");
 
-            await runner.RunAsync(code);
+            await Assert.ThrowsAsync<ScriptRuntimeErrorException>(() => runner.RunAsync(code));
         }
 
         [Fact]
-        public async Task AddConfigTwice_AddsNewConfig_Succeed()
+        public async Task Register_RegisterUnregisteredLaterVarAfterFail_Succeed()
         {
             var s = "abc";
             var t = new HelperObject();
             var code = "t.x += len(s)";
 
-            var runner = new PythonNetRunner();
+            using var runner = new PythonNetRunner();
             runner.Register(s, "s");
 
-            await Assert.ThrowsAsync<PythonException>(() => runner.RunAsync(code));
+            await Assert.ThrowsAsync<ScriptRuntimeErrorException>(() => runner.RunAsync(code));
 
             runner.Register(t, "t");
 
@@ -74,9 +77,9 @@ c = a + b";
         }
 
         [Fact]
-        public async Task RunWithTwoGlobalVariables_Succeed()
+        public async Task RunAsync_RunWithTwoGlobalVariables_Succeed()
         {
-            var runner = new PythonNetRunner();
+            using var runner = new PythonNetRunner();
             runner
                 .Register(1, "a")
                 .Register(2, "b");
@@ -85,12 +88,12 @@ c = a + b";
         }
 
         [Fact]
-        public async Task RunWithGlobalFunc_Succeed()
+        public async Task RunAsync_CallRegisteredFunction_Succeed()
         {
             int x = 0;
             var code = "t();";
 
-            var runner = new PythonNetRunner();
+            using var runner = new PythonNetRunner();
             runner.Register<Action>(() => x++, "t");
 
             await runner.RunAsync(code);
@@ -99,45 +102,45 @@ c = a + b";
         }
 
         [Fact]
-        public async Task RunInvalidCode_ThrowsException()
+        public async Task RunAsync_RunInvalidCode_ThrowsException()
         {
             var code = "1a = 1;";
 
-            var runner = new PythonNetRunner();
+            using var runner = new PythonNetRunner();
 
-            await Assert.ThrowsAsync<PythonException>(() => runner.RunAsync(code));
+            await Assert.ThrowsAsync<ScriptSyntaxErrorException>(() => runner.RunAsync(code));
         }
 
         [Fact]
-        public async void CodeThrowsAnException_SameExceptionIsThrowingFromRunner()
+        public async void RunAsync_CodeThrowsAnException_RunnerThrowsSameException()
         {
             var exceptionMessage = "Exception from user code";
             var code = $"raise Exception('{exceptionMessage}');";
 
-            var runner = new PythonNetRunner();
-            var exception = await Assert.ThrowsAsync<PythonException>(() =>
+            using var runner = new PythonNetRunner();
+            var exception = await Assert.ThrowsAsync<ScriptRuntimeErrorException>(() =>
                 runner.RunAsync(code));
 
             Assert.Equal(exceptionMessage, exception.Message);
         }
 
         [Fact]
-        public async Task ExposeFuncWithReturnValue_Succeed()
+        public async Task RunAsync_CallFunctionReturningValue_Succeed()
         {
             var code = @"
 c = add(1, 2)
 assert(c == 3)";
 
-            var runner = new PythonNetRunner();
+            using var runner = new PythonNetRunner();
             await runner
                 .Register<Func<int, int, int>>((a, b) => a + b, "add")
                 .RunAsync(code);
         }
 
         [Fact]
-        public async Task CallFunctionThrowingException_RunnerThrowsSameException()
+        public async Task RunAsync_CallFunctionThrowingException_RunnerThrowsSameException()
         {
-            var runner = new PythonNetRunner();
+            using var runner = new PythonNetRunner();
             var message = "Message from exception";
             runner.Register<Action>(() => throw new ArgumentException(message), "f");
 
@@ -151,12 +154,177 @@ assert(c == 3)";
         }
 
         [Fact]
-        public async void AddStructAsGlobals_Succeed()
+        public async Task Register_RegisterStruct_Succeed()
         {
-            var runner = new PythonNetRunner();
+            using var runner = new PythonNetRunner();
             await runner
                 .Register(new A(), "a")
                 .RunAsync("a.X += 1;");
+        }
+        
+        [Fact]
+        public async Task Evaluate_EvaluateValidExpression_Succeed()
+        {
+            using var runner = new PythonNetRunner();
+            var actual = await runner.EvaluateAsync<int>("1 + 1");
+            
+            Assert.Equal(2, actual);
+        }
+        
+        [Fact]
+        public async Task Evaluate_EvaluateWithContinuation_Succeed()
+        {
+            using var runner = new PythonNetRunner();
+            await runner.RunAsync("a = 1");
+            var actual = await runner.EvaluateAsync<int>("a + 1");
+            
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public async Task Evaluate_EvaluatePythonFunctionCall_Success()
+        {
+            using var runner = new PythonNetRunner();
+
+            await runner.RunAsync(@"
+def add(a, b):
+  return a + b;
+");
+            var val = await runner.EvaluateAsync<int>("add(1, 2)");
+            
+            Assert.Equal(3, val);
+        }
+        
+        [Fact]
+        public async Task RunAsync_MultipleTimesEachRunSharesGlobals_Success()
+        {
+            var code = @"x = 0";
+            
+            using var runner = new PythonNetRunner();
+
+            runner.Register<Func<int, int>>(x => x + 1, "Inc");
+            runner.Register<Action<int>>(x => Assert.Equal(2, x), "Check");
+
+            await runner.RunAsync(code);
+            await runner.RunAsync("x = Inc(x)");
+            await runner.RunAsync("x = Inc(x)");
+            await runner.RunAsync("Check(x)");
+        }
+        
+        [Fact]
+        public async Task Register_RegisteringNewGlobalVarBetweenRuns_Success()
+        {
+            using var runner = new PythonNetRunner();
+
+            runner.Register<Func<int, int>>(x => x + 1, "Inc");
+
+            await runner.RunAsync("x = 0");
+            await runner.RunAsync("x = Inc(x)");
+            await runner.RunAsync("x = Inc(x)");
+            
+            runner.Register<Action<int>>(x => Assert.Equal(2, x), "Check");
+            
+            await runner.RunAsync("Check(x)");
+        }
+        
+        [Fact]
+        public async Task RunAsync_RunWithContinuation_Success()
+        {
+            using var runner = new PythonNetRunner();
+            var code = @"
+x = 0
+def incr():
+  global x 
+  x += 1
+
+def check():
+  global x
+  assert(x == 2) 
+";
+
+            await runner.RunAsync(code);
+            await runner.RunAsync("incr()");
+            await runner.RunAsync("incr()");
+            await runner.RunAsync("check()");
+        }
+
+        [Fact]
+        public async Task Evaluate_EvaluateExpressionWithNetAndJsTypes_Succeed()
+        {
+            using var runner = new PythonNetRunner();
+
+            runner.Register(1, "x");
+            await runner.RunAsync("y = 2;");
+            var actual = await runner.EvaluateAsync<int>("x + y");
+            
+            Assert.Equal(3, actual);
+        }
+
+        [Fact]
+        public async Task Evaluate_EvaluateRegisteredString_Succeed() 
+        {
+            using var runner = new PythonNetRunner();
+
+            var str = "abc";
+            runner.Register(str, "s");
+            var actual = await runner.EvaluateAsync<string>("s");
+            
+            Assert.Equal(str, actual);
+        }
+
+        [Fact]
+        public async Task Register_NetAndJsIntegers_AreEqual()
+        {
+            using var runner = new PythonNetRunner();
+
+            runner.Register(1, "x");
+            await runner.RunAsync("y = 1;");
+            var actual = await runner.EvaluateAsync<bool>("x == y");
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public async Task Evaluate_RegisteredFunctionCall_Succeed() 
+        {
+            using var runner = new PythonNetRunner();
+
+            runner.Register<Func<string, string>>(name => "Hello " + name, "GetHello");
+            var result = await runner.EvaluateAsync<string>(@"GetHello('John')");
+            
+            Assert.Equal("Hello John", result);
+        }
+
+        [Fact]
+        public async Task RunAsync_CodeWithMixedIndentation_ThrowsSyntaxError()
+        {
+            using var runner = new PythonNetRunner();
+
+            var code = new StringBuilder()
+                .AppendLine("def f():")
+                .AppendLine("  a = 1")
+                .AppendLine("\tb = 2")
+                .AppendLine("  return a + b")
+                .AppendLine("c = f()")
+                .ToString();
+
+            await Assert.ThrowsAsync<ScriptSyntaxErrorException>(() => runner.RunAsync(code));
+        }
+        
+        [Fact]
+        public async Task RunAsync_CodeWithInconsistentIndentation_ThrowsSyntaxError()
+        {
+            using var runner = new PythonNetRunner();
+
+            var code = new StringBuilder()
+                .AppendLine("def f():")
+                .AppendLine("  a = 1")
+                .AppendLine("    b = 2")
+                .AppendLine("  return a + b")
+                .AppendLine("c = f()")
+                .ToString();
+
+            await Assert.ThrowsAsync<ScriptSyntaxErrorException>(() => runner.RunAsync(code));
         }
     }
 }
