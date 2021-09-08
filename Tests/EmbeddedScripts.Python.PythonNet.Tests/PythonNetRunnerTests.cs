3a3da708d35635dfe78006fe67c757258a17117a
Egor Medvedev <Egor.Medvedev@confirmit.com>
2021-09-08 14:36:36 +0300

    Remove commented code

--- Tests/EmbeddedScripts.Python.PythonNet.Tests/PythonNetRunnerTests.cs
+++ Tests/EmbeddedScripts.Python.PythonNet.Tests/PythonNetRunnerTests.cs
@@ -14,10 +14,7 @@ namespace EmbeddedScripts.Python.PythonNet.Tests
     {
         public PythonNetRunnerTests()
         {
-            // if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            //     PythonNetRunner.PythonDll = "/usr/lib/python3.8/config-3.8-x86_64-linux-gnu/libpython3.8.so";
-            
-            // fallback for local tests
+            // fallback for local machine tests
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && string.IsNullOrEmpty(PythonNetRunner.PythonDll))
                 PythonNetRunner.PythonDll = "C:/Python38/python38.dll";
         }
